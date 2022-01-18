46f5584b0b81be9d9736ff3be3cb5c2c593763a4
Michael Rogenmoser <michael@rogenmoser.us>
2022-01-18 12:28:01 +0100

    Ensure consistent VSIM

--- scripts/compile_vsim.sh
+++ scripts/compile_vsim.sh
@@ -15,6 +15,8 @@
 
 set -e
 
+[ ! -z "$VSIM" ] || VSIM=vsim
+
 bender script vsim -t test -t rtl --vlog-arg="-svinputport=compat" --vlog-arg="-override_timescale 1ns/1ps" > compile.tcl
 echo 'return 0' >> compile.tcl
-vsim -c -do 'exit -code [source compile.tcl]'
+${VSIM} -c -do 'exit -code [source compile.tcl]'
